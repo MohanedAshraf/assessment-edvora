0cac7a00122caa2e35904b1ed31bde7c155fea66
mohanedashraf <mohaned.ashraf.elsayed@gmail.com>
2022-02-08 09:28:33 +0200

    :lipstick: add productCard component

---
+++ pages/components/ProductCard.tsx
@@ -0,0 +1,33 @@
+export default function ProductCard() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen ">
+        <div className="flex flex-col max-w-sm p-5 rounded-xl bg-edvora-gray">
+          <div className="flex items-start ">
+            <img
+              className="w-24 h-24 mx-auto mb-3 rounded-lg shadow-lg "
+              src="https://images.unsplash.com/photo-1512054502232-10a0a035d672?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2080&q=80"
+              alt="product"
+            />
+            <div className="p-2 ml-5">
+              <h1 className="text-gray-200 text-md"> Product Name </h1>
+              <h3 className="mt-2 text-xs text-edvora-light-gray ">
+                Brand Name
+              </h3>
+              <p className="mt-4 text-xs text-gray-200">$ 29.99</p>
+            </div>
+          </div>
+          <div className="flex justify-between mt-2">
+            <h3 className="text-xs text-edvora-light-gray">Location</h3>
+            <h3 className="text-xs mr-7 text-edvora-light-gray">
+              Date:10:12:2021
+            </h3>
+          </div>
+          <div className="mt-2 text-xs text-edvora-light-gray">
+            Description of the product/item
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Filter from './components/Filter'
+import ProductCard from './components/ProductCard'
 
 export default function Home() {
   return (
@@ -7,8 +8,9 @@ export default function Home() {
       <Head>
         <title>Edvora</title>
       </Head>
-      <div className=" bg-edvora-gray font-body">
-        <Filter></Filter>
+      <div className=" bg-edvora-dark-gray font-body">
+        {/* <Filter></Filter> */}
+        <ProductCard />
       </div>
     </>
   )
